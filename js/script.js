022161288a9498af7906eac296cb2fd260cd5e79
Nishad <69951135+nishadnp@users.noreply.github.com>
2023-10-09 00:20:44 +0530

    feat: add feature to play a single round
    roundPlay() plays a round and returns the outcome.

--- js/script.js
+++ js/script.js
@@ -2,13 +2,43 @@ function getComputerChoice() {
     let choice = Math.floor(Math.random() * 3);
     switch(choice) {
         case 0:
-            return 'rock';
+            return 'ROCK';
         case 1:
-            return 'paper';
+            return 'PAPER';
         case 2:
-            return 'scissors';
+            return 'SCISSORS';
     }
 }
 
 
-console.log(getComputerChoice());
+
+function playRound(playerSelection, computerSelection) {
+    
+    console.log(playerSelection);
+    console.log(computerSelection)
+
+    if (playerSelection === computerSelection) 
+    {
+        const roundOutcome = 'TIE';
+        return roundOutcome;   
+    }
+    else if (playerSelection == 'ROCK' && computerSelection == 'SCISSORS' 
+    || playerSelection == 'SCISSORS' && computerSelection == 'PAPER' 
+    || playerSelection == 'PAPER' && computerSelection == 'ROCK')
+    {
+        const roundOutcome = `You Win! ${playerSelection} beats ${computerSelection}`;
+        return roundOutcome;
+    }
+    else
+    {
+        const roundOutcome = `You Lose! ${computerSelection} beats ${playerSelection}`;
+        return roundOutcome;
+    }
+}
+
+
+const computerSelection = getComputerChoice();
+const playerSelection = 'ROCK';
+
+
+console.log(playRound(playerSelection, computerSelection));
